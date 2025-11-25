8672a6a11be24057cd8d82fc469f01409ebd33a8
unknown <anudeepm006@gmail.com>
2025-11-25 11:50:05 +0530

    Added other functionality

--- index.js
+++ index.js
@@ -1,49 +1,78 @@
-const menu=[
-    {name:"Margarita",price:100},
-    {name:"Achar do Pyas",price:150},
-    {name:"Pasta Pizza",price:239},
-    {name:"Veg Loaded",price:335},
-    {name:"Golden Corn",price:250},
-
-]
-
-let cashInRegister=100
-const orderQue=[]
-
-function RandomId(){
-    return Math.floor(Math.random()*100)+1
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const menu = [
+    { id: RandomId(), name: "Margarita", price: 100 },
+    { id: RandomId(), name: "Achar do Pyas", price: 150 },
+    { id: RandomId(), name: "Pasta Pizza", price: 239 },
+    { id: RandomId(), name: "Veg Loaded", price: 335 },
+    { id: RandomId(), name: "Golden Corn", price: 250 },
+];
+let cashInRegister = 100;
+const orderQue = [];
+function RandomId() {
+    return Math.floor(Math.random() * 100) + 1;
 }
-
-function addNewPizza(newPizza){
-    menu.push(newPizza)
+function addNewPizza(newPizza) {
+    if (!newPizza.id) {
+        newPizza.id = RandomId();
+    }
+    menu.push(newPizza);
 }
-
-function placeOrder(pizza){
-    const [item]=menu.filter(item=>item.name==pizza)
-    if(item){
-        cashInRegister=cashInRegister+item.price
+function placeOrder(pizza) {
+    const [item] = menu.filter((item) => item.name == pizza);
+    if (item) {
+        cashInRegister = cashInRegister + item.price;
         let newOrder = { id: RandomId(), pizza: item.name, status: "ordered" };
-        orderQue.push(newOrder)
-        console.log(cashInRegister)
-        return newOrder
+        orderQue.push(newOrder);
+        console.log(cashInRegister);
+        return newOrder;
     }
-    else{
-        console.log("item could not be found")
+    else {
+        console.log("item could not be found");
+        return undefined;
     }
 }
-    
-function completeOrder(id){
-    const item=orderQue.find(item=>item.id==id)
-    if(item){
-        item.status="completed"
+function completeOrder(id) {
+    const item = orderQue.find((item) => item.id == id);
+    if (item) {
+        item.status = "completed";
         return item;
-    }else{
-        console.log("Item Not Found")
+    }
+    else {
+        console.log("Item Not Found");
+        return undefined;
     }
 }
-
-addNewPizza({name:"Chicken with Keema Pizza",price:542})
+function getPizzaDetails(identifier) {
+    let findPizza = orderQue.find(item => item.id == identifier || item.pizza == identifier);
+    if (!findPizza) {
+        console.log("Could not find the detail about the order");
+        return undefined;
+    }
+    return findPizza;
+}
+addNewPizza({ name: "Chicken with Keema Pizza", price: 542 });
 addNewPizza({ name: "Cheese Dominator", price: 242 });
-
-const {id}=placeOrder("Cheese Dominator")
-console.log(completeOrder(id))
+const order = placeOrder("Cheese Dominator");
+if (order) {
+    const { id } = order;
+    console.log(completeOrder(id));
+}
+const anotherOrder = placeOrder("Chicken with Keema Pizza");
+if (anotherOrder) {
+    const { id } = anotherOrder;
+    console.log(getPizzaDetails(id));
+}
+console.log("MENU");
+console.log(menu);
+console.log(`CASH IN REGISTER ${cashInRegister}`);
+// type Address={
+//   state:string
+//   country:string
+// }
+// type Person={
+//   name:string
+//   address?:Address
+// }
+// let data:Person={name:"asdasd"}
+//# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -5,30 +5,34 @@ type Order = {
 };
 
 type Pizza = {
+  id?:number
   name: string;
   price: number;
 };
 
-const menu:Pizza[] = [
-  { name: "Margarita", price: 100 },
-  { name: "Achar do Pyas", price: 150 },
-  { name: "Pasta Pizza", price: 239 },
-  { name: "Veg Loaded", price: 335 },
-  { name: "Golden Corn", price: 250 },
+const menu: Pizza[] = [
+  { id: RandomId(), name: "Margarita", price: 100 },
+  { id: RandomId(), name: "Achar do Pyas", price: 150 },
+  { id: RandomId(), name: "Pasta Pizza", price: 239 },
+  { id: RandomId(), name: "Veg Loaded", price: 335 },
+  { id: RandomId(), name: "Golden Corn", price: 250 },
 ];
 
 let cashInRegister = 100;
 const orderQue: Order[] = [];
 
-function RandomId() {
+function RandomId():number {
   return Math.floor(Math.random() * 100) + 1;
 }
 
-function addNewPizza(newPizza: Pizza) {
+function addNewPizza(newPizza: Pizza) :void{
+  if(!newPizza.id){
+    newPizza.id=RandomId();
+  }
   menu.push(newPizza);
 }
 
-function placeOrder(pizza: string){
+function placeOrder(pizza: string):Order | undefined{
   const [item] = menu.filter((item) => item.name == pizza);
   if (item) {
     cashInRegister = cashInRegister + item.price;
@@ -42,14 +46,24 @@ function placeOrder(pizza: string){
   }
 }
 
-function completeOrder(id: number) {
+function completeOrder(id: number):Order | undefined {
   const item = orderQue.find((item) => item.id == id);
   if (item) {
     item.status = "completed";
     return item;
   } else {
     console.log("Item Not Found");
+    return undefined
   }
+}
+
+function getPizzaDetails(identifier:string | number):Order | undefined{
+  let findPizza=orderQue.find(item=>item.id==identifier||item.pizza==identifier)
+  if(!findPizza){
+    console.log("Could not find the detail about the order")
+    return undefined;
+  }
+  return findPizza
 }
 
 addNewPizza({ name: "Chicken with Keema Pizza", price: 542 });
@@ -60,3 +74,26 @@ if(order){
     const {id}=order;
     console.log(completeOrder(id));
 }
+
+const anotherOrder=placeOrder("Chicken with Keema Pizza")
+if(anotherOrder){
+  const {id}=anotherOrder
+  console.log(getPizzaDetails(id))
+}
+
+console.log("MENU")
+console.log(menu)
+
+console.log(`CASH IN REGISTER ${cashInRegister}`)
+// type Address={
+//   state:string
+//   country:string
+// }
+
+// type Person={
+//   name:string
+//   address?:Address
+// }
+
+// let data:Person={name:"asdasd"}
+
